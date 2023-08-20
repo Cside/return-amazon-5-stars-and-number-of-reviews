1b40781333f40c8da908fceb7a738412d6f86235
Cside <cside.story@gmail.com>
2023-08-20 23:26:28 +0900

    for firefox bg page

--- scripts/build-firefox.sh
+++ scripts/build-firefox.sh
@@ -6,3 +6,5 @@ npx vite build --outDir dist-firefox
 
 cat dist-firefox/manifest.json | node ./scripts/manifest-firefox.js >manifest_firefox.json
 mv manifest_firefox.json dist-firefox/manifest.json
+
+cp background.html dist-firefox/background.html
